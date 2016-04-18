3dcd02a3e40637692dc1b9d2c525461a46f136dc
rupi9191 <rupi.9191@gmail.com>
2016-04-18 20:40:16 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+//var mongoose = require('mongoose');
+
+var mongodb;
+MongoClient.connect('mongodb://rupesh:rupesh@ds027335.mongolab.com:27335/heroku_1x6mqtdt', function(err, db) {
+  if (err) {
+    throw err;
+  }
+  mongodb = db;
+});
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 4000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+app.get('/users', function(req, res) {
+  // res.json({ message: 'hooray! welcome to our api!' }); 
+    mongodb.collection('usercollection').find().toArray(function(err, result) {
+    if (err) {
+      throw err;
+    }
+    res.json({ users: result}); 
+  });
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
+
+
