b47a3354f3aa2790ed4e104c9b432305bd3cf2fb
Rupesh <rupi.9191@gmail.com>
2016-04-27 00:24:31 +0530

    added lesson-3 ajax

---
+++ Assignments&Examples/Lesson5-Ajax/Assignment1/js/app.js
@@ -0,0 +1,13 @@
+/**
+* @ Create an object with distinct data types eg: string, interger, float, boolean, array
+* 
+*/
+var details = {
+    name: "Rupesh",
+    age: 25,
+    weight: 70.2,
+    single: true,
+    skills: ["Javascript","jQuery","AngularJS","EmberJS","BackboneJS"]
+}
+console.log(details)
+
